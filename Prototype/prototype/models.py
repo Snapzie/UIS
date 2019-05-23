717dc2852661e852f60327413fec0e9f17b12d06
kaapsi <casper@bresdahl.dk>
2019-05-23 23:31:36 +0200

    Prototype changes - model, view and controllers. Small change to scenarios

--- Prototype/prototype/models.py
+++ Prototype/prototype/models.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from prototype import conn, login_manager
 from flask_login import UserMixin
 
@@ -21,16 +20,7 @@ class Patients(tuple, UserMixin):
         self.password = user_data[1]
 
     def get_id(self):
-       return (self.CPR_number)
-
-class Diagnose(tuple):
-    def __init__(self, user_data):
-        self.illness = user_data[0]
-
-class Pharmacy(tuple):
-    def __init__(self, user_data):
-        self.name = user_data[0]
-        self.address = user_data[1]
+       return self.CPR_number
 
 class Prescription(tuple):
     def __init__(self, user_data):
@@ -46,11 +36,10 @@ class Prescription(tuple):
 
 class History(tuple):
     def __init__(self, user_data):
-        self.patient_CPR = user_data[0]
-        self.medicine_name = user_data[1]
-        self.medicine_concentration = user_data[2]
-        self.diagnosed_illness = user_data[3]
-        self.time = user_data[4]
+        self.medicine_name = user_data[0]
+        self.medicine_concentration = user_data[1]
+        self.renewal = user_data[2]
+        self.illness = user_data[3]
 
 class In_treatment_for(tuple):
     def __init__(self, user_data):
@@ -64,11 +53,11 @@ def select_Patient(CPR_number, conn):
     WHERE CPR_number = %s
     """
     cur.execute(sql, (CPR_number,))
-    user = Patients(cur.fetchone()) if cur.rowcount > 0 else None;
+    user = Patients(cur.fetchone()) if cur.rowcount > 0 else None
     cur.close()
     return user
 
-def select_Prescriptions(CPR_number, conn):
+def get_Prescriptions(CPR_number, conn):
     cur = conn.cursor()
     sql = """
     SELECT * FROM Prescription
@@ -81,23 +70,22 @@ def select_Prescriptions(CPR_number, conn):
     cur.close()
     return prescriptions
 
-def insert_History(prescription, conn):
-    cur = conn.cursor()
-    sql = """
-    INSERT INTO History(patient_CPR, medicine_name, medicine_concentration, diagnosed_illness, time) 
-    VALUES (%s, %s, %s, %s, CURRENT_DATE)
-    """
-    cur.execute(sql, (prescription.patient_CPR, prescription.medicine_name, prescription.medicine_concentration, prescription.illness))
-    cur.close()
-
 def get_History(CPR_number, conn):
+    p = get_Prescriptions(CPR_number, conn)
+    history = []
+    for perc in p:
+        history.append(History((perc.medicine_name, perc.medicine_concentration, perc.renewal, perc.illness)))
+    return history
+
+def get_Diagnoses(CPR_number, conn):
     cur = conn.cursor()
     sql = """
-    SELECT * from History WHERE patient_CPR = %s
-    """
-    cur.execute(sql,(CPR_number,))
-    history = []
+        SELECT * FROM In_treatment_for
+        WHERE patient_CPR = %s
+        """
+    cur.execute(sql, (CPR_number,))
+    diagnoses = []
     for row in cur.fetchall():
-        history.append(History(row))
+        diagnoses.append(In_treatment_for(row))
     cur.close()
-    return history
+    return diagnoses
