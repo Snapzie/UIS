9d4bf3b876b22b7315971ec47225adabc3100c25
kaapsi <casper@bresdahl.dk>
2019-05-27 21:19:39 +0200

    Added description of flask to report

--- Prototype/prototype/Main/routes.py
+++ Prototype/prototype/Main/routes.py
@@ -1,56 +1,56 @@
 from flask import render_template, url_for, flash, redirect, request, Blueprint
 from prototype import conn
-from prototype.forms import LoginForm
+from prototype.Main.forms import LoginForm
 from flask_login import login_user, current_user, logout_user
-from prototype.models import select_Patient, get_History, get_Diagnoses, get_Active_Prescriptions, insert_New_Renewed_Prescription, update_Active_Prescription
+from prototype.models import select_Patient, get_History, get_Diagnoses, get_Active_Prescriptions, insert_New_Renewed_Prescription
 
-Login = Blueprint('Login', __name__)
+Main = Blueprint('Main', __name__)
 
-@Login.route("/receptfornyelse")
+@Main.route("/receptfornyelse")
 def receptfornyelse():
     p = get_Active_Prescriptions(current_user.get_id(), conn)
     return render_template('receptfornyelse.html', percs=p)
 
-@Login.route('/renew/<med_name>/<med_conc>')
+@Main.route('/renew/<med_name>/<med_conc>')
 def renew(med_name, med_conc):
     insert_New_Renewed_Prescription(current_user.get_id(), med_name, med_conc, conn)
     p = get_Active_Prescriptions(current_user.get_id(), conn)
     flash('Receptfornyelse godkendt.', 'success')
     return redirect(url_for('.receptfornyelse', percs=p))
 
-@Login.route("/meddelser")
+@Main.route("/meddelser")
 def meddelelser():
     return render_template('meddelelser.html')
 
-@Login.route("/aftaler")
+@Main.route("/aftaler")
 def aftaler():
     return render_template('aftaler.html')
 
-@Login.route("/historik")
+@Main.route("/historik")
 def historik():
     h = get_History(current_user.get_id(), conn)
     return render_template('historik.html', history=h)
 
-@Login.route("/sundhedsdata")
+@Main.route("/sundhedsdata")
 def sundhedsdata():
     d = get_Diagnoses(current_user.get_id(), conn)
     return render_template('sundhedsdata.html', diagnoses=d)
 
-@Login.route("/fahjaelp")
+@Main.route("/fahjaelp")
 def fahjaelp():
     return render_template('fahjaelp.html')
 
-@Login.route("/")
-@Login.route("/profil")
+@Main.route("/")
+@Main.route("/profil")
 def profil():
     if not current_user.is_authenticated:
-        return redirect(url_for('Login.login'))
+        return redirect(url_for('Main.login'))
     return render_template('profil.html')
 
-@Login.route("/login", methods=['GET', 'POST'])
+@Main.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('Login.profil'))
+        return redirect(url_for('Main.profil'))
     form = LoginForm()
     if form.validate_on_submit():
         user = select_Patient(form.id.data, conn)
@@ -58,12 +58,12 @@ def login():
             login_user(user)
             flash('Login successful.', 'success')
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('Login.profil'))
+            return redirect(next_page) if next_page else redirect(url_for('Main.profil'))
         else:
             flash('Login Unsuccessful. Please check identifier and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
-@Login.route("/logout")
+@Main.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('Login.login'))
+    return redirect(url_for('Main.login'))
